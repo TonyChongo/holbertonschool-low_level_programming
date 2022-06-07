44b55a579450fc8f182bbaebbf9a09a00cedf583
ChongLeangUENG <5129@holbertonschool.com>
2022-06-07 03:47:50 -0700

    Print combination

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = '0'; m <= '9'; m++)
+	{
+		putchar(m);
+		if (m != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
