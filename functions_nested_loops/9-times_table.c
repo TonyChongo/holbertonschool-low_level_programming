74690b479d0ea22a5457045e5296732e5d31c049
ChongLeangUENG <5129@holbertonschool.com>
2022-06-08 11:31:09 -0700

    Times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ * Return: Nothing
+ */
+void times_table(void)
+{
+	int u, c, l;
+
+	for (u = 0; u <= 9; u++)
+	{
+		for (c = 0; c <= 9; c++)
+		{
+			l = u * c;
+
+			if ((l / 10) == 0)
+			{
+				if (c != 0)
+					_putchar(' ');
+				_putchar(l + '0');
+
+				if (c == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((l / 10) + '0');
+				_putchar((l % 10) + '0');
+				if (c ==9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
