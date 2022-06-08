0f53b5934a72b0b749b9fd1b87b8fb1bc3e12c0f
ChongLeangUENG <5129@holbertonschool.com>
2022-06-08 09:00:27 -0700

    Print alphabet x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int l;
+
+	for (l = 0; l < 10; l++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
