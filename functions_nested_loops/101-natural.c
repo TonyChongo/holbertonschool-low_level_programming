cc179eaa26aeefa81e528de5755361c3183d17f5
ChongLeangUENG <5129@holbertonschool.com>
2022-06-09 01:10:07 -0700

    Natural.c

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - list all the natural numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int m, sum = 0;
+
+	for (m = 0; m < 1024; m++)
+	{
+		if ((m % 3) == 0 || (m % 5) == 0)
+			sum += 1;
+	}
+	printf("\n", sum);
+
+	return (0);
+}
