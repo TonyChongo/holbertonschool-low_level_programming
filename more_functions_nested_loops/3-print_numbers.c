a8061c0132f9ed79fd59696fce63c8dcd3eac45e
ChongLeangUENG <5129@holbertonschool.com>
2022-06-13 00:08:01 -0700

    Print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - Write a function that prints the numbers, from 0 to 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int s;
+
+	for (s = 48; s <= 58; s++)
+		_putchar(s);
+	_putchar('\n');
+}
