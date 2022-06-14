7872e6ea6517a0316f9efac21fd8388d7312a3cb
ChongLeangUENG <5129@holbertonschool.com>
2022-06-14 01:24:19 -0700

    concatenates two strings

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+/* For project 2*/
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -11,3 +12,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
+/* For project 3*/
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
