51b13e22bcce9db21cb909ce5b8ed246296f9db6
ChongLeangUENG <5129@holbertonschool.com>
2022-06-17 07:45:35 -0700

    Write a function that convert a string to an integer

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,6 +12,11 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
+/* Project 2: Advance*/
+
+int _atoi(char *s);
+
+
 /* For project 3*/
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
