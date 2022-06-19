3b79e38e6ccc06e539c715a80b098df98f49fa13
ChongLeangUENG <5129@holbertonschool.com>
2022-06-19 15:27:06 -0700

    Puts recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Write a function that prints a string.
+ * @s: A string char
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	while (*s)
+		_putchar(*s++);
+	_putchar('\n');
+}
