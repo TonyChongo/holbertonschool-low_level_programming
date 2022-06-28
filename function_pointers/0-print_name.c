cbbe155c6988f588533e231e708172915959455b
ChongLeangUENG <5129@holbertonschool.com>
2022-06-28 00:30:50 -0700

    Print name in function pointers

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,3 +1,16 @@
 #include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * print_name - Write a function that prints a name.
+ * @name: Pointer of char
+ * @f: pointer of void
+ * Return: Always success.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+	f(name);
+}
