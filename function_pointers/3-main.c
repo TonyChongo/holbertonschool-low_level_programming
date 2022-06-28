2507dcfcce1e019cb3b541bc5486ea69ab162990
ChongLeangUENG <5129@holbertonschool.com>
2022-06-28 08:46:53 -0700

    Write a program that performs simple operations

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -18,12 +18,6 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
 	opera = get_op_func(argv[2]);
 	i = atoi(argv[1]);
 	j = atoi(argv[3]);
