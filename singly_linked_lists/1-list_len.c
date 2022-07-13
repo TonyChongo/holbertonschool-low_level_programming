e38b374915b4bfd72da2882d2c130e9a67e7ac4e
ChongLeangUENG <5129@holbertonschool.com>
2022-07-13 00:08:45 -0700

    Commit list length

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - function that returns the number of elements in a linked list.
+ * @h: Singly linked list
+ * Return: number of elements in a linked list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elements;
+
+	elements = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		elements++;
+	}
+	return (elements);
+}
