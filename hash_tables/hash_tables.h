a1e1967b83d67047324a7a2f257302182638e54b
ChongLeangUENG <leangcnrp777@gmail.com>
2022-08-19 00:26:21 -0700

    Commit to debugging key to index

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,6 +1,12 @@
 #ifndef _HASH_TABLES_H_
 #define _HASH_TABLES_H_
 
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
