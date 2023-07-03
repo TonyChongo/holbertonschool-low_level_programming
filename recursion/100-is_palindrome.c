4f00a1321ed1b9dd8dc1f065d5d5d3ae11eebc48
TonyMeme <tonychong203@gmail.com>
2023-07-03 15:08:09 +0200

    Update loop is not allow

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -10,14 +10,16 @@
 
 int is_palindrome(char *s)
 {
-    int length = strlen(s);
-    int i, j;
+	int length = strlen(s);
+	int i, j;
 
-    for (i = 0, j = length - 1; i < j; i++, j--) {
-        if (s[i] != s[j]) {
-            return 0;
-        }
-    }
+	for (i = 0, j = length - 1; i < j; i++, j--)
+	{
+		if (s[i] != s[j])
+		{
+			return 0;
+		}
+	}
 
-    return 1;
+	return 1;
 }
